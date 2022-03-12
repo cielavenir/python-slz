91547e1581583c05a033183c8977a202f1b9d447
T.Yamada <cielartisan@gmail.com>
2022-03-12 14:42:02 +0900

    Update pyslz.cpp

--- src/pyslz.cpp
+++ src/pyslz.cpp
@@ -37,7 +37,8 @@ public:
             //py::gil_scoped_release release;
 			fprintf(stderr, "2\n");fflush(stderr);
             //written = slz_encode(&strm, &out[0], buffer, length, 1);
-        }
+  written = slz_finish(&strm, &out[0]);
+      }
         //out.resize(written);
 		fprintf(stderr, "ooo_%d\n", written);fflush(stderr);
         return py::bytes(out.data(), written);
