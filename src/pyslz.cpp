45668d46229008525a96caf45d403a55e333c3a6
T.Yamada <cielartisan@gmail.com>
2022-03-13 14:25:50 +0900

    Update pyslz.cpp

--- src/pyslz.cpp
+++ src/pyslz.cpp
@@ -5,8 +5,6 @@
 #endif
 extern "C" {
 #include "libslz/src/slz.h"
-struct slz_stream *slz_alloc();
-void slz_free(struct slz_stream *s);
 }
 
 #if defined(_WIN32) || (!defined(__GNUC__) && !defined(__clang__))
@@ -18,52 +16,31 @@ using namespace pybind11::literals;
 
 class slz_compressobj{
     slz_stream strm;
-    slz_stream *pstrm;
     std::string out;
     int outsize;
 public:
     slz_compressobj(int level=1, int format=SLZ_FMT_DEFLATE): outsize(0){
-printf("pypypy %d\n",sizeof(slz_stream));
-#if 0
-//defined(_WIN32) || (!defined(__GNUC__) && !defined(__clang__))
-        pstrm = slz_alloc();
-#else
-        pstrm = &strm;
-#endif
-        slz_init(pstrm, level, format);
-    }
-    ~slz_compressobj(){
-#if 0
-//defined(_WIN32) || (!defined(__GNUC__) && !defined(__clang__))
-        slz_free(pstrm);
-#endif
+        slz_init(&strm, level, format);
     }
     py::bytes compress(const py::bytes &obj){
-		fprintf(stderr, "0\n");fflush(stderr);
-		fprintf(stderr, "iii_%d\n", py::len(obj));fflush(stderr);
         int tempoutsize = py::len(obj)+py::len(obj)/16;
         if(outsize < tempoutsize){
             outsize = tempoutsize;
-            out.resize(outsize);
+            out.reserve(outsize);
         }
         //out.resize(tempoutsize);
         size_t written = 0;
         {
-			fprintf(stderr, "1\n");fflush(stderr);
             char *buffer = nullptr;
             ssize_t length = 0;
             PYBIND11_BYTES_AS_STRING_AND_SIZE(obj.ptr(), &buffer, &length);
-            //py::gil_scoped_release release;
-			fprintf(stderr, "2\n");fflush(stderr);
-            written = slz_encode(pstrm, &out[0], buffer, length, 1);
-  //written = slz_finish(pstrm, &out[0]);
-      }
+            py::gil_scoped_release release;
+            written = slz_encode(&strm, &out[0], buffer, length, 1);
+        }
         //out.resize(written);
-		fprintf(stderr, "ooo_%d\n", written);fflush(stderr);
         return py::bytes(out.data(), written);
     }
     py::bytes flush(){
-		fprintf(stderr, "f0\n");fflush(stderr);
         int tempoutsize = 12;
         if(outsize < tempoutsize){
             outsize = tempoutsize;
@@ -72,10 +49,8 @@ printf("pypypy %d\n",sizeof(slz_stream));
         //out.resize(tempoutsize);
         size_t written = 0;
         {
-			fprintf(stderr, "f1\n");fflush(stderr);
-            //py::gil_scoped_release release;
-			fprintf(stderr, "f2\n");fflush(stderr);
-            written = slz_finish(pstrm, &out[0]);
+            py::gil_scoped_release release;
+            written = slz_finish(&strm, &out[0]);
         }
         //out.resize(written);
         return py::bytes(out.data(), written);
