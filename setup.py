f39ef5cf7a73f889d8dd8ca8c6edd3aaabc4b99e
cielavenir <cielartisan@gmail.com>
2022-03-17 13:47:38 +0900

    foo

--- setup.py
+++ setup.py
@@ -22,9 +22,11 @@ class build_ext_hook(build_ext, object):
             if sys.maxsize < 1<<32:
                 gcc = 'i686-w64-mingw32-gcc'
                 gxx = 'i686-w64-mingw32-g++'
+                plat = 'win32'
             else:
                 gcc = 'x86_64-w64-mingw32-gcc'
                 gxx = 'x86_64-w64-mingw32-g++'
+                plat = 'win-amd64'
             subprocess.check_call([gcc, '-c', '-DPRECOMPUTE_TABLES=1', '-o', 'slz.o', '-O2', 'src/libslz/src/slz.c'])
             subprocess.check_call([gcc, '-c', '-o', 'chkstk.o', 'src/chkstk.S'])
             if sys.version_info < (3,5):
@@ -35,19 +37,19 @@ class build_ext_hook(build_ext, object):
                     '-I', sysconfig.get_paths()['platinclude'],
                     '-I', pybind11.get_include(),
                     'src/pyslz.cpp'])
-                # ext.extra_objects.append('pyslz.o')
-                subprocess.check_call(['mkdir', '-p', 'build\lib.win32-2.7'])
-                subprocess.check_call(['ls', join(dirname(sys.executable))])
-                # https://stackoverflow.com/a/48360354/2641271
-                d = Distribution()
-                b = d.get_command_class('build_ext')(d)
-                b.finalize_options()
-                print(b.library_dirs)
-                subprocess.check_call(['ls'] + b.library_dirs)
-                subprocess.check_call([gxx, '-shared', '-o', 'build\lib.win32-2.7\slz.pyd',
-                    'pyslz.o', 'slz.o', 'chkstk.o', '-lpython27',
-                ]+sum((['-L', dir] for dir in b.library_dirs), []))
-                return
+                if False:
+                    ext.extra_objects.append('pyslz.o')
+                else:
+                    pydname = 'build/lib.%s-%d.%d/%s.pyd'%(plat, sys.hexversion // 16777216, sys.hexversion // 65536 % 256, ext.name.replace('.', '/')
+                    subprocess.check_call(['mkdir', '-p', dirname(pydname)])
+                    # https://stackoverflow.com/a/48360354/2641271
+                    d = Distribution()
+                    b = d.get_command_class('build_ext')(d)
+                    b.finalize_options()
+                    subprocess.check_call([gxx, '-shared', '-o', pydname),
+                        'pyslz.o', 'slz.o', 'chkstk.o', '-lpython27',
+                    ]+sum((['-L', dir] for dir in b.library_dirs), []))
+                    return
             else:
                 ext.sources.append('src/pyslz.cpp')
             ext.extra_objects.extend(['slz.o', 'chkstk.o'])
