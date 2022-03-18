cd34008f4963dac66442f9bcd796bb926f0f61db
T.Yamada <cielartisan@gmail.com>
2022-03-19 02:32:21 +0900

    add Win 2.7 wheel (#3)
    * temp
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * typo
    
    * aaa
    
    * aaa
    
    * aaa
    
    * aaa
    
    * aaa
    
    * wip
    
    * aaa
    
    * foo
    
    * typo
    
    * foo
    
    * foo
    
    * foo
    
    * bar
    
    * foo
    
    * bla
    
    * xxx
    
    * foo
    
    * foo
    
    * zzzz
    
    * fff
    
    * foo
    
    * foo
    
    * fix
    
    * ddd
    
    * foo
    
    * foo
    
    * sss
    
    * ttt
    
    * foo
    
    * foo
    
    * foo
    
    * ttt
    
    * switch to clang
    
    * teardown
    
    * bump version
    
    * reduce diff
    
    * fix typo
    
    * temp

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import sys
 import platform
+from os.path import join
 from os.path import dirname
 from os.path import abspath
+from os.path import isfile
 sys.path.append(dirname(abspath(__file__)))
 import monkeypatch_distutils
 import subprocess
 
 from setuptools import setup
+from setuptools.dist import Distribution
 from setuptools.command.build_ext import build_ext
 try:
     from pybind11.setup_helpers import Pybind11Extension
@@ -18,19 +21,50 @@ class build_ext_hook(build_ext, object):
         if platform.system() == 'Windows':
             if sys.maxsize < 1<<32:
                 msiz = '-m32'
+                plat = 'win32'
+                win64flags = []
             else:
                 msiz = '-m64'
-            subprocess.check_call(['gcc', msiz, '-c', '-DPRECOMPUTE_TABLES=1', '-o', 'slz.o', '-O2', 'src/libslz/src/slz.c'])
-            subprocess.check_call(['gcc', msiz, '-c', '-o', 'chkstk.o', 'src/chkstk.S'])
+                plat = 'win-amd64'
+                win64flags = ['-DMS_WIN64=1']
+            subprocess.check_call(['clang', msiz, '-c', '-DPRECOMPUTE_TABLES=1', '-o', 'slz.o', '-O2', 'src/libslz/src/slz.c'])
+            subprocess.check_call(['clang', msiz, '-c', '-o', 'chkstk.o', 'src/chkstk.S'])
+            if sys.version_info < (3,5):
+                import sysconfig
+                import pybind11
+                subprocess.check_call(['clang++', msiz, '-c', '-o', 'pyslz.o', '-O2',
+                    '-DHAVE_UINTPTR_T=1',
+                    '-I', sysconfig.get_paths()['include'],
+                    '-I', sysconfig.get_paths()['platinclude'],
+                    '-I', pybind11.get_include(),
+                    'src/pyslz.cpp']+win64flags)
+                ext.extra_objects.append('pyslz.o')
+                if True:
+                    ext.extra_objects.extend(['slz.o'])
+                    pydpath = 'build/lib.%s-%d.%d/%s.pyd'%(plat, sys.hexversion // 16777216, sys.hexversion // 65536 % 256, ext.name.replace('.', '/'))
+                    subprocess.check_call(['mkdir', '-p', dirname(pydpath)])
+                    libname = 'python%d%d.lib'%(sys.hexversion // 16777216, sys.hexversion // 65536 % 256)
+                    # https://stackoverflow.com/a/48360354/2641271
+                    d = Distribution()
+                    b = d.get_command_class('build_ext')(d)
+                    b.finalize_options()
+                    libpath = next(join(dir, libname) for dir in b.library_dirs if isfile(join(dir, libname)))
+                    print(libpath)
+                    subprocess.check_call([
+                        'clang++', msiz, '-shared', '-o', pydpath,
+                    ]+ext.extra_objects+[libpath])
+                    return
+            else:
+                ext.sources.append('src/pyslz.cpp')
             ext.extra_objects.extend(['slz.o', 'chkstk.o'])
         else:
-            ext.sources.append('src/libslz/src/slz.c')
+            ext.sources.extend(['src/pyslz.cpp', 'src/libslz/src/slz.c'])
         build_ext.build_extension(self, ext)
 
 ext_modules = [
     Pybind11Extension(
-        "slz",
-        sources=['src/pyslz.cpp'],
+        name="slz",
+        sources=[],
         extra_objects=[],
         extra_compile_args=['-O2'],
         extra_link_args=['-s'],
@@ -41,7 +75,7 @@ setup(
     name='slz',
     description='a (light) binding for libslz',
     long_description=open("README.md").read(),
-    version='0.0.0.4',
+    version='0.0.0.5',
     url='https://github.com/cielavenir/python-slz',
     license='MIT',
     author='cielavenir',
